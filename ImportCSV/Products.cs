4074b57fd19506a5b40b31c816395c1d99861b28
Marcelo Beani <marcelo_beani@hotmail.com>
2022-11-07 05:51:04 -0300

    Cleaning the code and trating the lines with no data.

--- ImportCSV/Products.cs
+++ ImportCSV/Products.cs
@@ -17,20 +17,46 @@ namespace ImportCSV
 
         public Products(string csvLines)
         {
-            string[] data = csvLines.Split(',');
+                string[] data = csvLines.Split(',');
 
-            //addig the parts chopped into my class properties
-            ProductName = data[0];
-            ProductPrice = Convert.ToDouble(data[1]);
-            ProductBrand = data[2];
-            ProductWeight = Convert.ToDouble(data[3]);
 
+                //addig the parts chopped into my class properties
+                try
+                {
+                    ProductName = data[0];
+                }
+                catch { }
+
+                try
+                {
+                    ProductPrice = Convert.ToDouble(data[1]);
+                }
+                catch { }
+
+                try
+                {                   
+                    ProductBrand = data[2];
+                }
+                catch { }
+
+                try
+                {
+                    ProductWeight = Convert.ToDouble(data[3]);
+                }
+                catch { }
+            
         }
 
+        //I used the override method where so all when I ask the main 
+        //class to print the product List, the method will do this for all the products.
+        //make it easier to print on screen;
         public override string ToString()
         {
 
-            string str = $"{ProductName} | {ProductPrice} | {ProductBrand} | {ProductWeight}";              
+            string str = $"Description:{ProductName} " +
+                $"Price: ${ProductPrice} " +
+                $"Brand: {ProductBrand} " +
+                $"Weight {ProductWeight}";              
             return str;
         }
 
--- ImportCSV/Program.cs
+++ ImportCSV/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace ImportCSV
@@ -14,71 +15,28 @@ namespace ImportCSV
             ///Reading all the file for the first time
             ///*reading all file
             ///string csvAllText = System.IO.File.ReadAllText(@"C:\\Users\\c052108\\OneDrive - Yara International ASA\\Desktop\\VS Projects\Products.csv");
-            
+
             //Read the file into lines
             string[] csvLines = System.IO.File.ReadAllLines(@"C:\Users\c052108\OneDrive - Yara International ASA\Desktop\VS Projects\Products.csv");
 
-            ///First Attemp Using lists    
-            ///* creating lists to store the values i've chopped from the lines
-            ///- First Attemp:
-            ///var productName = new List<string>();
-            ///var productPrice = new List<double>();
-           /// var productBrand = new List<string>();
-           /// var productWeight = new List<double>();
-            ///
-            ///reading the lines and chopping them into colluns
-            ///Like this it reads also the header, and we dont want that.
-            ///for (int i = 0; i < csvLines.Length; i++)
-            
             var products = new List<Products>();
 
             for (int i = 1; i < csvLines.Length; i++)
             {       
-            ///First Attemp Using lists    
-                    ///Adding the ellements i choopped on Lists;
-                    ///string[] rawLines = csvLines[i].Split(',');
-                    ///productName.Add(rawLines[0]);
-                    /// double p = Convert.ToDouble(rawLines[1]);
-                    ///productPrice.Add(p);
-                    ///productBrand.Add(rawLines[2]);
-                    ///double w = Convert.ToDouble(rawLines[3]);
-                    ///productWeight.Add(w);*/
                     
                 Products prod = new Products(csvLines[i]);
+                if (prod.ProductBrand != null)
                 products.Add(prod);
 
             }
 
-            Console.WriteLine("Product Name | Product Price | Product Brand | Product Weight");
+            Console.WriteLine("Products Imported:");
 
             for (int i = 0; i < products.Count; i++)
             {
                 
                 Console.WriteLine(products[i]);
             }
-
-            /// First Attemp Using the Lists
-            ///*Console.WriteLine("Product Name");
-            ///for (int i = 0; i < productName.Count; i++)
-            /// {
-            ///     Console.WriteLine(productName[i]);
-            /// }
-            /// Console.WriteLine("Product Price");
-            /// for (int i = 0; i < productPrice.Count; i++)
-            /// {
-            ///     Console.WriteLine(productPrice[i]);
-            /// }           
-            ///Console.WriteLine("Produc Brand");
-            ///for (int i = 0; i < productBrand.Count; i++)
-            ///{
-            ///   Console.WriteLine(productBrand[i]);
-            /// }   
-            /// Console.WriteLine("Product Weight");
-            /// for (int i = 0; i < productWeight.Count; i++)
-            ///{
-            ///     Console.WriteLine(productWeight[i]);
-            /// }*/
-            ///
 
             Console.WriteLine("Total of Products: " + products.Count) ;        
             Console.ReadKey();
